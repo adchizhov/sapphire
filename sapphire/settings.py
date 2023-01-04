9133e9af4e0403530987939ceb862a6408dc59fa
achizhov <achizhov@more.tv>
2023-01-04 03:35:50 +0300

    fix leaflet marker and removed collected_static folder from git

--- sapphire/settings.py
+++ sapphire/settings.py
@@ -144,7 +144,6 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
